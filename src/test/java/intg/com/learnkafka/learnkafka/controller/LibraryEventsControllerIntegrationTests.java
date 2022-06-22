ca1c9171798abd11b76bd998c8848d60491db2ee
reese <mauriceyancey@utexas.edu>
2022-06-21 23:58:23 -0400

    Added in an Integration Test

---
+++ src/test/java/intg/com/learnkafka/learnkafka/controller/LibraryEventsControllerIntegrationTests.java
@@ -0,0 +1,41 @@
+package com.learnkafka.learnkafka.controller;
+
+import domain.Book;
+import domain.LibraryEvent;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LibraryEventsControllerIntegrationTests {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void postLibraryEvent(){
+        Book book = Book.builder()
+                .bookId(123)
+                .bookAuthor("Reese Yancey")
+                .bookName("Kafka using Spring Boot")
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book)
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("content-type", MediaType.APPLICATION_JSON.toString());
+        HttpEntity<LibraryEvent> request = new HttpEntity<>(libraryEvent,headers);
+
+        ResponseEntity<LibraryEvent> responseEntity= restTemplate.exchange("/v1/libraryevent", HttpMethod.POST, request, LibraryEvent.class);
+
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+
+    }
+}
